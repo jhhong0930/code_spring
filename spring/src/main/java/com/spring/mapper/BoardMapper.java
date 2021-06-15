44c1e03fd9cddedd6cf5a99180b3f9f0a3986497
jhhong0930 <jhhong0930@gmail.com>
2021-06-16 01:01:28 +0900

    update project chap09

---
+++ spring/src/main/java/com/spring/mapper/BoardMapper.java
@@ -0,0 +1,23 @@
+package com.spring.mapper;
+
+import com.spring.domain.BoardVO;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface BoardMapper {
+
+//    @Select("SELECT * FROM SPRING.BOARD WHERE BNO > 0")
+    public List<BoardVO> getList();
+
+    public void insert(BoardVO board);
+
+    public void insertSelectKey(BoardVO board);
+
+    public BoardVO read(Long bno);
+
+    public int delete(Long bno);
+
+    public int update(BoardVO board);
+
+}
